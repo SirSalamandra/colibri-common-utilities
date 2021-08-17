0800986186f1b26443d8ae1a0163752f40c663a6
zgabrielsalesz <gabriel.s479@hotmail.com>
2021-08-17 10:59:48 -0300

    teste pull request3

--- src/validations.ts
+++ src/validations.ts
@@ -1,5 +1,13 @@
 export let validations = {
-  getValidations(a: string): string {
-    return "validations";
-  }
+  isNumeric(value: string): boolean {
+    return /^\d+$/.test(value);
+  },
+  isLetter(value: string): boolean {
+    return /[A-Z]|[a-z]+/.test(value);
+  },
+  isValidEmail(email: string): boolean {
+    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+  },
 }
+
