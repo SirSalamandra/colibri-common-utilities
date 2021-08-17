474f2b555a96449a618598e959690f510cd81857
zgabrielsalesz <gabriel.s479@hotmail.com>
2021-08-17 10:50:12 -0300

    Teste pull request

--- src/converters.ts
+++ src/converters.ts
@@ -2,6 +2,8 @@ export let converters = {
   getNumbers(text: string) {
     if (!text) return "";
     return text.replace(/\D+/g, '');
-    
+  },
+  updateObject(obj) {
+    return JSON.parse(JSON.stringify(obj));
   }
 }
